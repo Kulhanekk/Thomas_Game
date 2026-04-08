f2e5691b11b0830d6a3632323f62e28c64bd7666
Jan Kulhanek <kulhaja9@fel.cvut.cz>
2026-04-08 11:53:18 +0200

    arduino buzzer example and basic notes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include <notes.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define BUZZER_PIN 8
+
+void setup()
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+	for (int note = 0; note < N_OF_NOTES; note++)
+	{
+
+		// to calculate the note duration, take one second divided by the note type.
+		// e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+		int note_duration = 1000 / note_durations[note];
+		tone(BUZZER_PIN, melody[note], note_duration);
+
+		// to distinguish the notes, set a minimum time between them.
+		// the note's duration + 30% seems to work well:
+		int note_pause = note_duration * 1.30;
+		delay(note_pause);
+		// stop the tone playing:
+		noTone(BUZZER_PIN);
+	}
 }
